dae2547ee0001f4daa13c04546c2f6b6923dee42
ナム Nam Nguyen <nam@ring.md>
2016-12-11 22:55:51 +0700

    Hide completed tasked. Using ReactiveDict

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,42 @@
+import { ReactiveDict } from 'meteor/reactive-dict'
+
 import { Tasks } from '../api/tasks.js'
 import './task.js'
 import './body.html'
 
+Template.body.onCreated( function bodyOnCreated(){
+    this.state = new ReactiveDict()
+  }
+);
+
 Template.body.helpers({
   tasks(){
+    const instance = Template.instance()
+    if(instance.state.get('hideCompleted')) {
+      return Tasks.find(
+        {
+          checked:{
+            $ne:true
+          }
+        },
+        {
+          sort:{
+            createdAt: -1
+          }
+        }
+      ).fetch()
+    }
     return Tasks.find({},{
       sort: { createdAt: -1 }
     }).fetch()
   },
+  incompletedCount() {
+    return Tasks.find({
+      checked: {
+        $ne: true
+      }
+    }).count()
+  }
 });
 
 Template.body.events({
@@ -28,4 +57,7 @@ Template.body.events({
     // Clear form
     target.text.value = '';
   },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked)
+  }
 });

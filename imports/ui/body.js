0f0ae720140743c66d546c31643bdad500c5fa1f
ナム Nam Nguyen <nam@ring.md>
2016-12-11 22:30:33 +0700

    Beable to delete tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,12 @@
-import { Template } from 'meteor/templating'
 import { Tasks } from '../api/tasks.js'
+import './task.js'
 import './body.html'
-
 
 Template.body.helpers({
   tasks(){
-    return Tasks.find().fetch()
+    return Tasks.find({},{
+      sort: { createdAt: -1 }
+    }).fetch()
   },
 });
 
